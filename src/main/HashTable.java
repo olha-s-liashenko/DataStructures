c3c6a2f6af7d2de2359e04ce1ca8d03bd7d93259
Olha Liashenko <olha.s.liashenko@gmail.com>
2020-09-29 19:41:17 +0300

    Implement Iterator for HashTable. Refactor. Implement tests.

--- src/main/HashTable.java
+++ src/main/HashTable.java
@@ -1,11 +1,19 @@
-package main.HashTable;
+package main;
 
-import java.util.Hashtable;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
 import java.util.LinkedList;
 
-public class HashTable <K, V> {
+public class HashTable <K, V> implements Iterable<HashTable.Entry> {
 
-    private class Entry<K, V> {
+    @Override
+    public Iterator iterator() {
+        return new HashTableIterator();
+    }
+
+    public class Entry<K, V> {
         K key;
         V value;
 
@@ -13,21 +21,51 @@ public class HashTable <K, V> {
             this.key = key;
             this.value = value;
         }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return "Entry{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    '}';
+        }
     }
 
     private LinkedList<Entry<K,V>>[] arr;
-    private final float DEFAULT_LOAD_FACTOR = 0.75f;
-    protected int size = 0;
-    private int capacity = 0;
-    private float loadFactor = 0;
+    private final BigDecimal DEFAULT_LOAD_FACTOR = new BigDecimal(0.75);
+    protected int capacity = 0;
+    private int actualSize = 0;
+    private BigDecimal loadFactor = new BigDecimal(0);
 
     public HashTable () {
-        int initSize = 16;
+        this(16);
+    }
+
+    public HashTable (int capacity) {
+        this(null, capacity);
+    }
+
+    public HashTable (HashTable<K, V> src, int capacity) {
+        int initSize = capacity;
         this.arr = new LinkedList[initSize];
-        this.size = initSize;
+        if (src != null) {
+            for (Entry<K, V> entry : src) {
+                put(entry.key, entry.value);
+            }
+        }
+        this.capacity = initSize;
     }
 
     public void put(K key, V value) {
+        if (key == null) throw new IllegalArgumentException("Key cannot be null. Please, try again.");
         Entry <K, V> entry = new Entry<>(key, value);
         int index = getIndex(key.hashCode());
         LinkedList <Entry<K, V>> list = arr[index];
@@ -36,9 +74,9 @@ public class HashTable <K, V> {
             arr[index] = list;
         }
         list.add(entry);
-        capacity++;
+        actualSize++;
         loadFactor = countLoadFactor();
-        if (loadFactor > DEFAULT_LOAD_FACTOR) {
+        if (loadFactor.compareTo(DEFAULT_LOAD_FACTOR) == 1) {
             rehash();
         }
     }
@@ -58,23 +96,25 @@ public class HashTable <K, V> {
     }
 
     public int size() {
-        return capacity;
+        return actualSize;
     }
 
     private int getIndex(int hash) {
-        return hash % size;
+        return (hash & 0x7FFFFFFF) % capacity;
     }
 
-    private int countLoadFactor() {
-        return size / capacity;
+    private BigDecimal countLoadFactor() {
+        return new BigDecimal((float) actualSize / capacity);
     }
 
     private void rehash() {
-        LinkedList<Entry<K, V>>[] oldArr = new LinkedList[size];
-        System.arraycopy(arr, 0, oldArr, 0, size);
-        size *= 2;
-        arr = new LinkedList[size];
+        LinkedList<Entry<K, V>>[] oldArr = new LinkedList[capacity];
+        System.arraycopy(arr, 0, oldArr, 0, capacity);
+        capacity *= 2;
+        actualSize = 0;
+        arr = new LinkedList[capacity];
         for (LinkedList<Entry<K, V>> list : oldArr) {
+            if (list == null) continue;
             for (Entry<K, V> entry : list) {
                 put(entry.key, entry.value);
             }
@@ -82,4 +122,53 @@ public class HashTable <K, V> {
 
     }
 
+    public class HashTableIterator implements Iterator<Entry> {
+
+        private final int currentSize;
+        private int counter = 0;
+        Entry<K, V>[] resultingArr;
+
+        HashTableIterator() {
+            currentSize = size();
+            LinkedList<Entry<K, V>>[] table = arr;
+            int counter = 0;
+            resultingArr = new Entry[actualSize];
+            for (LinkedList<Entry<K, V>> list : table) {
+                if (list == null) continue;
+                for (Entry<K, V> entry : list) {
+                    resultingArr[counter] = entry;
+                    counter++;
+                }
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return counter < resultingArr.length;
+        }
+
+        @Override
+        public Entry<K, V> next() {
+            if (currentSize != size()) throw new ConcurrentModificationException();
+            Entry<K, V> result = null;
+            if (counter < resultingArr.length) {
+                result = resultingArr[counter];
+                counter++;
+            } else {
+                counter = 0;
+            }
+            return result;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "HashTable{" +
+                "arr=" + Arrays.toString(arr) +
+                ", DEFAULT_LOAD_FACTOR=" + DEFAULT_LOAD_FACTOR +
+                ", capacity=" + capacity +
+                ", actualSize=" + actualSize +
+                ", loadFactor=" + loadFactor +
+                '}';
+    }
 }
--- src/test/TestHashTable.java
+++ src/test/TestHashTable.java
@@ -1,2 +1,47 @@
-package test;public class TestHashTable {
+package test;
+
+import main.HashTable;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestHashTable {
+
+    private class Contact {
+        String name = "";
+        String tel = "";
+
+        public Contact(String name, String tel) {
+            this.name = name;
+            this.tel = tel;
+        }
+    }
+
+    @Test
+    public void testHashTable() {
+        String[] names = {"Adel", "Bob", "David", "Eva", "Helen", "Kate", "Luke", "Mary", "Nathan", "Peter", "Sarah", "Timothy", "Yannis"};
+        String[] tels = {"0771234532", "0453424323", "0785372348", "0785473847", "0674937584", "0674958767", "0657493847", "0875647364", "0896756432", "0476584756", "0908768574", "0785647343", "0675849876"};
+        Contact[] contacts = new Contact[names.length];
+        for (int i = 0; i < contacts.length; i++) {
+            contacts[i] = new Contact(names[i], tels[i]);
+        }
+
+
+        HashTable<String, String> contactsTable = new HashTable<>();
+        for (int i = 0;  i < contacts.length; i++) {
+            contactsTable.put(contacts[i].name, contacts[i].tel);
+        }
+
+        assertEquals(13, contactsTable.size());
+        assertEquals("0908768574", contactsTable.get("Sarah"));
+
+        Object prevEntry = null;
+        System.out.println(contactsTable.toString());
+
+        for (Object entry : contactsTable) {
+            System.out.println(entry.toString());
+            assertTrue(entry != null);
+        }
+    }
 }
